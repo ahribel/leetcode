a11250c748af1c611ca64ede71290dd49173dd0f
Jakub Wilde <jakub.wilde@wp.pl>
2025-04-24 19:08:28 +0200

    Naming convention fixes

---
+++ problems/easy/1768_merge_strings_alternately/cpp/solution_1768.h
@@ -0,0 +1,13 @@
+#ifndef SOLUTION_1768_H
+#define SOLUTION_1768_H
+
+#include <string>
+
+using namespace std;
+
+namespace s1 {
+    string mergeAlternately(string word1, string word2);
+}
+
+
+#endif
